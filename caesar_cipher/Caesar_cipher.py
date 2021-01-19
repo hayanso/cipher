6749a1e9899f3df32be60371e0686d6162659f75
baek <sjbaek5411@gmail.com>
2021-01-19 09:49:52 +0900

    write caesar

---
+++ caesar_cipher/Caesar_cipher.py
@@ -0,0 +1,21 @@
+####### Declaration #######
+eng_alphabet_small = "abcdefghijklmnopqrstuvwxyz"
+eng_alphabet_big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+cipher_text = ""
+
+####### Definition #######
+def caesar_encript(plain_text, shift_num):
+    global cipher_text
+    for index_num in range(0, len(plain_text)):
+        index_str = plain_text[index_num]
+        if index_str.islower() == True:
+            position_num = eng_alphabet_small.index(index_str)
+            replace_position_num = (position_num + shift_num) % 26
+            cipher_text += eng_alphabet_small[replace_position_num]
+        elif index_str.isupper() == True:
+            position_num = eng_alphabet_big.index(index_str)
+            replace_position_num = (position_num + shift_num) % 26
+            cipher_text += eng_alphabet_big[replace_position_num]
+        elif index_str.isalpha() == False:
+            cipher_text += index_str
+    return cipher_text
---
+++ caesar_cipher/Caesar_cipher_decrypt.py
@@ -0,0 +1,25 @@
+####### Declaration #######
+eng_alphabet_small = "abcdefghijklmnopqrstuvwxyz"
+eng_alphabet_big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+plain_text = ""
+out_come_dic = {}
+
+####### Definition #######
+def caesar_decrypt(cipher_text):
+    global plain_text
+    for shift_num in range(26):
+        for index_num in range(0, len(cipher_text)):
+            index_str = cipher_text[index_num]
+            if index_str.islower() == True:
+                position_num = eng_alphabet_small.index(index_str)
+                replace_position_num = (position_num - shift_num) % 26
+                plain_text += eng_alphabet_small[replace_position_num]
+            elif index_str.isupper() == True:
+                position_num = eng_alphabet_big.index(index_str)
+                replace_position_num = (position_num - shift_num) % 26
+                plain_text += eng_alphabet_big[replace_position_num]
+            elif index_str.isalpha() == False:
+                plain_text += index_str
+        out_come_dic[shift_num] = plain_text
+        plain_text = ""
+    return out_come_dic
